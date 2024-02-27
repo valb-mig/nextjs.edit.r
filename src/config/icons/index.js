03bd429dee0fb8ccb302db82df12240787070637
valb-mig <valb.mig@gmail.com>
2024-02-26 23:38:13 -0300

    :wrench:(config): Setting config functions

--- src/config/icons/index.js
+++ src/config/icons/index.js
@@ -25,35 +25,46 @@ import {
 import {
     FaMagnifyingGlass,
     FaTrash,
-    FaFile
+    FaFile,
+    FaGear
 } from 'react-icons/fa6';
 
 import {
-    FaPlus
+    FaPlus,
+    FaPhp,
+    FaMarkdown
 } from 'react-icons/fa';
 
 import { 
     IoMdCloseCircle 
 } from "react-icons/io";
 
+import { 
+    IoLogoJavascript 
+} from "react-icons/io5";
+
 const Icon = {
 
-    Tag:    AiFillTag,
-    Sun:    BsFillSunFill,
-    Moon:   BsFillMoonFill,
-    Close:  IoMdCloseCircle,
-    Grid:   AiOutlineMenu,
-    User:   BiSolidUserCircle,
-    Search: FaMagnifyingGlass,
-    Home:   AiFillHome,
-    Plus:   FaPlus,
-    Folder: BiSolidFolderOpen,
-    Dot:    BsDot,
-    Hash:   MdOutlineGrid3X3,
-    Trash:  FaTrash,
-    Check:  AiOutlineCheck,
-    Drag:   MdDragIndicator,
-    File:   FaFile
+    Tag:        AiFillTag,
+    Sun:        BsFillSunFill,
+    Moon:       BsFillMoonFill,
+    Close:      IoMdCloseCircle,
+    Grid:       AiOutlineMenu,
+    User:       BiSolidUserCircle,
+    Search:     FaMagnifyingGlass,
+    Home:       AiFillHome,
+    Plus:       FaPlus,
+    Folder:     BiSolidFolderOpen,
+    Dot:        BsDot,
+    Hash:       MdOutlineGrid3X3,
+    Trash:      FaTrash,
+    Check:      AiOutlineCheck,
+    Drag:       MdDragIndicator,
+    File:       FaFile,
+    Javascript: IoLogoJavascript,
+    Php:        FaPhp,
+    Md:         FaMarkdown,
+    Gear:       FaGear
 }
 
 export default Icon;
