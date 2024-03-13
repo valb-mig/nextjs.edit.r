6490e910f9b176ecf9959867be3aae526ba78b8d
valb-mig <valb.mig@gmail.com>
2024-03-12 22:11:46 -0300

    :sparkles:(feat): Sidebar close/open button

--- src/app/components/Sidebar/config/Body.js
+++ src/app/components/Sidebar/config/Body.js
@@ -1,5 +1,5 @@
 const SidebarBody = ({ children }) => {
-  return <div className="flex w-100 py-2 flex-col gap-2">{children}</div>;
+    return <div className="flex w-100 py-2 flex-col gap-2">{children}</div>;
 };
 
 export default SidebarBody;
--- src/app/components/Sidebar/config/Root.js
+++ src/app/components/Sidebar/config/Root.js
@@ -1,16 +1,42 @@
+import React, { useState } from "react";
+
+import Icon from "@/config/icons";
+import Button from "@/app/components/Button";
+
 const SidebarRoot = ({ children }) => {
-  return (
-    <div className="flex flex-col justify-content-between bg-dark-0 w-[12rem] h-full p-2 border-r-[1px] border-dark-3">
-      <div className="flex flex-col">{children}</div>
-      <span className="flex no-underline justify-content-center w-100 text-dark-3">
-        <a href="https://github.com/valb-mig/edit.r" target="__blank">
-          <small>
-            <i>made by valb.mig</i>
-          </small>
-        </a>
-      </span>
-    </div>
-  );
+    const [showSidebar, setShowSidebar] = useState(true);
+
+    return (
+        <div className="flex relative z-[999]">
+            {showSidebar && (
+                <div className="flex flex-col justify-content-between bg-dark-0 w-[12rem] h-full p-2 border-r-[1px] border-dark-3">
+                    <div className="flex flex-col">{children}</div>
+                    <span className="flex no-underline justify-content-center w-100 text-dark-3">
+                        <a
+                            href="https://github.com/valb-mig/edit.r"
+                            target="__blank"
+                        >
+                            <small>
+                                <i>made by valb.mig</i>
+                            </small>
+                        </a>
+                    </span>
+                </div>
+            )}
+            <div className="flex absolute right-[-2rem] top-[0.5rem]">
+                <Button
+                    Icon={
+                        showSidebar ? (
+                            <Icon.SidebarRight />
+                        ) : (
+                            <Icon.SidebarLeft />
+                        )
+                    }
+                    OnClick={() => setShowSidebar(!showSidebar)}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default SidebarRoot;
